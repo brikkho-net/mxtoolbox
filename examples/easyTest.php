a921d8787c8aa3d97aa1ed7424cc61b39bab2683
hexim <spacek@best-net.cz>
2016-03-23 15:24:58 +0100

    test

--- examples/easyTest.php
+++ examples/easyTest.php
@@ -3,46 +3,49 @@ use MxToolbox\MxToolbox;
 use MxToolbox\Exceptions\MxToolboxRuntimeException;
 use MxToolbox\Exceptions\MxToolboxLogicException;
 
-require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'../src/MxToolbox/autoload.php';
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '../src/MxToolbox/autoload.php';
 
-class easyTest extends MxToolbox {
-	
-	public function configure() {
-		$this
-			->setDigPath('/usr/bin/dig')
-			->pushDNSResolverIP('127.0.0.1')
-			->buildBlacklistHostnamesArray();
-	}
+class easyTest extends MxToolbox
+{
 
-	/**
-	 * @param $addr
+    public function configure()
+    {
+        $this
+            ->setDigPath('/usr/bin/dig')
+            ->pushDNSResolverIP('127.0.0.1')
+            ->buildBlacklistHostnamesArray();
+    }
+
+    /**
+     * @param $addr
      */
-	public function testMyIPAddress($addr) {
+    public function testMyIPAddress($addr)
+    {
 
-		try {
-		
-			var_dump($this->getTestBlacklistsArray()->);
-		
-			// check IP address
-			//	$mxt->checkAllrBLS($addr);
-			/*
-			* Show result
-			* Structure:
-			* []['blHostName'] = DNSBL host name
-			* []['blPositive'] = true if IP addres have the positive check
-			* []['blPositiveResult'][] = array of URL address if IP address have the positive check
-			* []['blResponse'] = true if DNSBL host name is alive and send test response before test
-			*/
-			//	var_dump($mxt->getCheckResult());
-		}
-		catch ( MxToolboxRuntimeException $e ) {
-			echo $e->getMessage() . PHP_EOL;
-		}
-		catch ( MxToolboxLogicException $e ) {
-			echo $e->getMessage() . PHP_EOL;
-		}
-		
-	}
-	
-}	
+        try {
 
+            var_dump($this->getTestBlacklistsArray());
+
+            // check IP address
+            //	$mxt->checkAllrBLS($addr);
+            /*
+            * Show result
+            * Structure:
+            * []['blHostName'] = DNSBL host name
+            * []['blPositive'] = true if IP addres have the positive check
+            * []['blPositiveResult'][] = array of URL address if IP address have the positive check
+            * []['blResponse'] = true if DNSBL host name is alive and send test response before test
+            */
+            //	var_dump($mxt->getCheckResult());
+        } catch (MxToolboxRuntimeException $e) {
+            echo $e->getMessage() . PHP_EOL;
+        } catch (MxToolboxLogicException $e) {
+            echo $e->getMessage() . PHP_EOL;
+        }
+
+    }
+
+}
+
+$test = new easyTest();
+$test->testMyIPAddress('194.8.253.5');
--- src/MxToolbox/MxToolbox.php
+++ src/MxToolbox/MxToolbox.php
@@ -1,7 +1,6 @@
 <?php
 namespace MxToolbox;
 
-use MxToolbox\AbstractMxToolbox;
 use MxToolbox\Exceptions\MxToolboxLogicException;
 use MxToolbox\Exceptions\MxToolboxRuntimeException;
 use MxToolbox\FileSystem\BlacklistsHostnameFile;
@@ -17,140 +16,149 @@ use MxToolbox\FileSystem\BlacklistsHostnameFile;
  * @version 0.0.2-dev
  *
  */
-class MxToolbox extends AbstractMxToolbox {
+class MxToolbox extends AbstractMxToolbox
+{
 
-	
-	public function __construct() {
-		$this->configure();
-	}
-	/**
-	 * Configures the current command.
-	 */
-	public function configure() {
-		
-	}
-	
-	/**
-	 * Load blacklist and create test array
-	 * @param array reference $blacklistHostNames - optional (you may use your own blaclist array)
-	 * @return this
-	 * @throws MxToolboxRuntimeException;
-	 * @throws MxToolboxLogicException;
-	 */
-	public function buildBlacklistHostnamesArray(&$blacklistHostNames = NULL) {
-		if ( is_null($blacklistHostNames) ) {
-			try {
-				$hosts = new BlacklistsHostnameFile();
-				$hosts->loadBlacklistsFromFile('blacklistsAlive.txt');
-				$this->setTestResultArray($hosts->getBlacklistsHostNames());
-				return $this;
-			}
-			catch ( MxToolboxRuntimeException $e ) {
-				if ( $e->getCode()==400 ) {
-					$hosts->loadBlacklistsFromFile('blacklists.txt');
-					$this->setTestResultArray( $hosts->getBlacklistsHostNames() );
-					$hosts->makeAliveBlacklistFile( $this->setDnsblResponse()->getTestResultArray() );
-					return $this;
-				}
-				return $e;
-			}
-		}
-		$this->setTestResultArray($blacklistHostNames);
-		return $this;
-	}
-	
-	/**
-	 * Get test blacklist array
-	 * @return array
-	 * @throws MxToolboxLogicException
-	 */
-	public function &getTestBlacklistsArray() {
-		if ( is_array($this->getTestResultArray()) && count($this->getTestResultArray()) > 0 )
-			return $this->getTestResultArray();
-		throw new MxToolboxLogicException('Array is empty. Call buildBlacklistHostnamesArray() first.');
-	}
-	
-	//sprintf('The command defined in "%s" cannot have an empty name.', get_class($this))
-	
-	/**
-	 * Check all (use only alive rBLS - fast check!)
-	 * @param string $addr
-	 * @return boolean - TRUE if process is done, FALSE on non valid IP address or if the blacklist is not loaded
-	 */
-	public function checkAllDnsbl($addr) {
-		$this->checkDigPath();
-		if ( $this->validateIPAddress($addr) && count($this->testResult) > 0 ) {
-			foreach ($this->testResult as &$blackList) {
-				if ( $this->checkOnerBLSARecord($addr, $blackList['blHostName']) ) {
-					$blackList['blPositive'] = true;
-					$blackList['blPositiveResult'] = $this->getUrlForPositveCheck($addr, $blackList['blHostName']);
-				}
-			}
-			unset($blackList);
-			return true;
-		}
-		$this->testResult = array();
-		return false;
-	}
-	
-	/**
-	 * Get PTR record
-	 * @return mixed string|bool
-	 */
-	public function getPTR() {
-		if ( !empty($this->recordPTR) )
-			return $this->recordPTR;
-		return false;
-	}
-	
-	/**
-	 * Get domain name
-	 * @return mixed string|bool
-	 */
-	public function getDomainName() {
-		if ( !empty($this->domainName) )
-			return $this->domainName;
-		return false;
-	}
 
-	/**
-	 * Get MX records from domain name
-	 * @param string $hostName
-	 * @return mixed - ARRAY with MX records or FALSE
-	 */
-	public function getMXRecords($hostName) {
-		if ( $this->checkHostName($hostName) ) {
-			$ptr = dns_get_record( $hostName, DNS_MX );
-			if ( isset($ptr[0]['target']) ) {
-				$mxRecords = array();
-				foreach ($ptr as $mx)
-					$mxRecords[] = $mx['target'];
-				return $mxRecords;
-			}
-		}
-		return false;
-	}
+    public function __construct()
+    {
+        $this->configure();
+    }
 
-	/**
-	 * Check if IP address have a PTR record
-	 * @param string $addr
-	 * @return boolean
-	 */
-	public function checkExistPTR($addr) {
-		$this->recordPTR = '';
-		$this->domainName = '';
-		if ( ! $this->validateIPAddress($addr) )
-			return false;
-		$ptr = dns_get_record( $this->reverseIP($addr) . '.in-addr.arpa.', DNS_PTR );
-		if ( isset($ptr[0]['target']) ) {
-			$regs = array();
-			$this->recordPTR = $ptr[0]['target'];
-			if ( preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $ptr[0]['target'], $regs) )
-				$this->domainName = $regs['domain'];
-			return true;
-		}
-		return false;
-	}
-	
+    /**
+     * Configures the current command.
+     */
+    public function configure()
+    {
+    }
+
+    /**
+     * Load blacklist and create test array
+     * @param array $blacklistHostNames - optional (you may use your own blaclist array)
+     * @return $this
+     * @throws MxToolboxRuntimeException;
+     * @throws MxToolboxLogicException;
+     */
+    public function buildBlacklistHostnamesArray(&$blacklistHostNames = NULL)
+    {
+        if (is_null($blacklistHostNames)) {
+            try {
+                $hosts = new BlacklistsHostnameFile();
+                $hosts->loadBlacklistsFromFile('blacklistsAlive.txt');
+                $this->setTestResultArray($hosts->getBlacklistsHostNames());
+                return $this;
+            } catch (MxToolboxRuntimeException $e) {
+                if ($e->getCode() == 400) {
+                    $hosts->loadBlacklistsFromFile('blacklists.txt');
+                    $this->setTestResultArray($hosts->getBlacklistsHostNames());
+                    $hosts->makeAliveBlacklistFile($this->setDnsblResponse()->getTestResultArray());
+                    return $this;
+                }
+                return $e;
+            }
+        }
+        $this->setTestResultArray($blacklistHostNames);
+        return $this;
+    }
+
+    /**
+     * Get test blacklist array
+     * @return array
+     * @throws MxToolboxLogicException
+     */
+    public function &getTestBlacklistsArray()
+    {
+        if (is_array($this->getTestResultArray()) && count($this->getTestResultArray()) > 0)
+            return $this->getTestResultArray();
+        throw new MxToolboxLogicException('Array is empty. Call buildBlacklistHostnamesArray() first.');
+    }
+
+    //sprintf('The command defined in "%s" cannot have an empty name.', get_class($this))
+
+    /**
+     * Check all (use only alive rBLS - fast check!)
+     * @param string $addr
+     * @return boolean - TRUE if process is done, FALSE on non valid IP address or if the blacklist is not loaded
+     */
+    public function checkAllDnsbl($addr)
+    {
+        $this->checkDigPath();
+        if ($this->validateIPAddress($addr) && count($this->testResult) > 0) {
+            foreach ($this->testResult as &$blackList) {
+                if ($this->checkDnsblPtrRecord($addr, $blackList['blHostName'])) {
+                    $blackList['blPositive'] = true;
+                    $blackList['blPositiveResult'] = $this->getUrlForPositveCheck($addr, $blackList['blHostName']);
+                }
+            }
+            unset($blackList);
+            return true;
+        }
+        $this->testResult = array();
+        return false;
+    }
+
+    /**
+     * Get PTR record
+     * @return mixed string|bool
+     */
+    public function getPTR()
+    {
+        if (!empty($this->recordPTR))
+            return $this->recordPTR;
+        return false;
+    }
+
+    /**
+     * Get domain name
+     * @return mixed string|bool
+     */
+    public function getDomainName()
+    {
+        if (!empty($this->domainName))
+            return $this->domainName;
+        return false;
+    }
+
+    /**
+     * Get MX records from domain name
+     * @param string $hostName
+     * @return mixed - ARRAY with MX records or FALSE
+     */
+    public function getMXRecords($hostName)
+    {
+        if ($this->checkHostName($hostName)) {
+            $ptr = dns_get_record($hostName, DNS_MX);
+            if (isset($ptr[0]['target'])) {
+                $mxRecords = array();
+                foreach ($ptr as $mx)
+                    $mxRecords[] = $mx['target'];
+                return $mxRecords;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check if IP address have a PTR record
+     * @param string $addr
+     * @return boolean
+     */
+    public function checkExistPTR($addr)
+    {
+        $this->recordPTR = '';
+        $this->domainName = '';
+        if (!$this->validateIPAddress($addr))
+            return false;
+        $ptr = dns_get_record($this->reverseIP($addr) . '.in-addr.arpa.', DNS_PTR);
+        if (isset($ptr[0]['target'])) {
+            $regs = array();
+            $this->recordPTR = $ptr[0]['target'];
+            if (preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $ptr[0]['target'], $regs))
+                $this->domainName = $regs['domain'];
+            return true;
+        }
+        return false;
+    }
+
 
 }
