ef6deca79a1d2862cac03b8f3bc7a938c9df297a
heximcz <spacek@best-net.cz>
2016-04-04 02:19:45 +0200

    update examples

--- examples/myOwnBlackList.php
+++ examples/myOwnBlackList.php
@@ -49,25 +49,35 @@ class myOwnBlackList extends MxToolbox
     public function testMyIPAddress($addr)
     {
 
-        // Checks IP address on all DNSBL
+        /*
+         * Get test array prepared for check (without any test results)
+         */
+        //var_dump($this->getBlacklistsArray());
+
+        /*
+         * Check IP address on all DNSBL
+         */
         $this->checkIpAddressOnDnsbl($addr);
 
         /*
-         * getBlacklistsArray() structure:
-         * []['blHostName'] = dnsbl hostname
-         * []['blPositive'] = true if IP address have the positive check
-         * []['blPositiveResult'] = array() array of a URL addresses if IP address have the positive check
-         * []['blResponse'] = true if DNSBL host name is alive and send test response before test
-         * []['blQueryTime'] = false or response time of a last dig query
+         *  Get the same array but with a check results
+         * 
+         *  Return structure:
+         *  []['blHostName'] = dnsbl hostname
+         *  []['blPositive'] = true if IP address have the positive check
+         *  []['blPositiveResult'] = array() array of a URL addresses if IP address have the positive check
+         *  []['blResponse'] = true if DNSBL host name is alive or DNSBL responded during the test
+         *  []['blQueryTime'] = false or response time of a last dig query
          */
+
         var_dump($this->getBlacklistsArray());
 
-        /* Cleaning old results before next test
+        /*
+         * Cleaning old results - REQUIRED only in loop before next test
          * TRUE = check responses for all DNSBL again (default value)
-         * FALSE = only cleaning old results (response = true)
+         * FALSE = only cleaning old results ([blResponse] => true)
          */
         $this->cleanBlacklistArray(false);
-
 
     }
 
