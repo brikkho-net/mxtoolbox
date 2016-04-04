47422203ec932b3c7c452f891fad52a903aea7fd
heximcz <spacek@best-net.cz>
2016-04-04 02:17:31 +0200

    update examples

--- examples/example.php
+++ examples/example.php
@@ -34,38 +34,35 @@ class example extends MxToolbox
      */
     public function testMyIPAddress($addr)
     {
-        // Checks IP address on all DNSBL
+        /*
+         * Get test array prepared for check (without any test results)
+         */
+        //var_dump($this->getBlacklistsArray());
+
+        /*
+         * Check IP address on all DNSBL
+         */
         $this->checkIpAddressOnDnsbl($addr);
 
         /*
-         * getBlacklistsArray() structure:
-         * []['blHostName'] = dnsbl hostname
-         * []['blPositive'] = true if IP address have the positive check
-         * []['blPositiveResult'] = array() array of a URL addresses if IP address have the positive check
-         * []['blResponse'] = true if DNSBL host name is alive and send test response before test
-         * []['blQueryTime'] = false or response time of a last dig query
+         *  Get the same array but with a check results
+         * 
+         *  Return structure:
+         *  []['blHostName'] = dnsbl hostname
+         *  []['blPositive'] = true if IP address have the positive check
+         *  []['blPositiveResult'] = array() array of a URL addresses if IP address have the positive check
+         *  []['blResponse'] = true if DNSBL host name is alive or DNSBL responded during the test
+         *  []['blQueryTime'] = false or response time of a last dig query
          */
+
         var_dump($this->getBlacklistsArray());
 
-        /* Cleaning old results before next test
+        /*
+         * Cleaning old results - REQUIRED only in loop before next test
          * TRUE = check responses for all DNSBL again (default value)
-         * FALSE = only cleaning old results (response = true)
+         * FALSE = only cleaning old results ([blResponse] => true)
          */
         $this->cleanBlacklistArray(false);
-
-        // get array with dns resolvers
-        var_dump($this->getDnsResolvers());
-
-        // get DIG path
-        var_dump($this->getDigPath());
-
-        /* Get additional information for IP address
-         * Array structure:
-         * ['domainName']
-         * ['ptrRecord']
-         * ['mxRecords'][array]
-         */
-        var_dump($this->getDomainInformation($addr));
 
         // Get SMTP server diagnostics responses
         // '64.12.91.197' is any public SMTP server
@@ -75,6 +72,20 @@ class example extends MxToolbox
             'mxtool@example.com',
             'test@example.com'
         ));
+
+        /* Get additional information for IP address
+         * Array structure:
+         * ['domainName']
+         * ['ptrRecord']
+         * ['mxRecords'][array]
+         */
+        var_dump($this->getDomainInformation($addr));
+
+        // get array with dns resolvers
+        var_dump($this->getDnsResolvers());
+
+        // get DIG path
+        var_dump($this->getDigPath());
 
     }
 
