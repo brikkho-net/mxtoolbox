4a16291583802dd2e060c8b2f991c28c5113c485
heximcz <spacek@best-net.cz>
2016-04-05 17:58:31 +0200

    version 0.0.6

--- src/MxToolbox/MxToolbox.php
+++ src/MxToolbox/MxToolbox.php
@@ -8,7 +8,7 @@
  * @license https://opensource.org/licenses/MIT
  * @link https://github.com/heximcz/mxtoolbox
  * @link https://best-hosting.cz
- * @version 0.0.5
+ * @version 0.0.6
  */
 namespace MxToolbox;
 
