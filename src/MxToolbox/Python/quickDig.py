a08d3777bb6737ddad8edceeb8174770f462390f
heximcz <spacek@best-net.cz>
2016-04-16 01:32:35 +0200

    python multiprocess initial commit

---
+++ src/MxToolbox/Python/quickDig.py
@@ -0,0 +1,39 @@
+import json, sys, subprocess, multiprocessing
+
+
+class DataObject:
+    def __init__(self, params):
+        self.domain, self.ip, self.digPath, self.resolver = params
+
+
+def run_process(obj_instance):
+    domain, ip, digPath, resolver = obj_instance.domain, obj_instance.ip, obj_instance.digPath, obj_instance.resolver
+    processed = []
+    processed.append(subprocess.check_output([digPath + ' @' + resolver + ' ' + ip + '.' + domain + ' A'], shell=True))
+    return processed
+
+
+def load(path):
+    with open(path, "r") as file:
+        blacklist = file.read().splitlines()
+    return blacklist
+
+
+if __name__ == '__main__':
+    """ sys.argv[1] = test ip address (must be reversed)
+        sys.argv[2] = dig path (/usr/bin/dig)
+        sys.argv[3] = resolver ip address
+        sys.argv[4] = blacklistsAlive.txt path
+        Usage example: python ./quickDig.py 2.0.0.127 /usr/bin/dig 127.0.0.1 /<path>/blacklistsAlive.txt
+    """
+    try:
+        blacklist = load(sys.argv[4])
+        objList = []
+        for domain in blacklist:
+            objList.append(DataObject((domain, sys.argv[1], sys.argv[2], sys.argv[3])))
+        pool = multiprocessing.Pool()
+        results = pool.map(run_process, objList)
+        print(json.dumps(results))
+    except Exception as e:
+        print ('error')
+
